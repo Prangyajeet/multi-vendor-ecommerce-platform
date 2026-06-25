ed630dcf1410dd94573f199f8909296ff363a858
bhagya23-git <bhagyashreemap@gmail.com>
2026-06-25 23:30:42 +0530

    Implemented registration form with API integration

--- frontend/src/components/common/Input.jsx
+++ frontend/src/components/common/Input.jsx
@@ -1,15 +1,21 @@
 function Input({
   label,
+  name,
   type = "text",
   placeholder,
+  value,
+  onChange,
 }) {
   return (
     <div className="flex flex-col gap-2">
       <label>{label}</label>
 
       <input
+        name={name}
         type={type}
         placeholder={placeholder}
+        value={value}
+        onChange={onChange}
         className="border rounded-lg p-2"
       />
     </div>
